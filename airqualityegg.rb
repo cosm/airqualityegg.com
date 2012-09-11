2fea34e949208863b93188c3cfe13662fda59b62
Levent Ali <lebreeze@gmail.com>
2012-09-11 11:45:27 +0000

    Use activate endpoint and store response in session

--- airqualityegg.rb
+++ airqualityegg.rb
@@ -6,6 +6,7 @@ require 'cosm-rb'
 class AirQualityEgg < Sinatra::Base
 
   configure do
+    enable :sessions
     $product_id = ENV['PRODUCT_ID']
     $api_key = ENV['API_KEY']
     $api_url = ENV['API_URL'] || Cosm::Client.base_uri
@@ -13,8 +14,10 @@ class AirQualityEgg < Sinatra::Base
     raise "PRODUCT_ID not set" if $product_id.nil?
     raise "API_KEY not set" if $api_key.nil?
     raise "API_URL not set" if $api_url.nil?
-  end
+    puts "WARN: You should set a SESSION_SECRET" unless ENV['SESSION_SECRET']
 
+    set :session_secret, ENV['SESSION_SECRET'] || 'airqualityegg_session_secret'
+  end
 
   get '/' do
     erb :home
@@ -22,12 +25,14 @@ class AirQualityEgg < Sinatra::Base
 
   post '/register' do
     redirect '/' if params[:serial].blank?
-    url = "#{$api_url}/v2/device_models/#{$product_id}/devices/#{params[:serial]}"
+    url = "#{$api_url}/v2/products/#{$product_id}/devices/#{params[:serial]}/activate"
     response = Cosm::Client.get(url, :headers => {"X-ApiKey" => $api_key})
-
     begin
-      feed_id = MultiJson.load(response.body)['device']['feed_id']
-      redirect feed_id ? "/egg/#{feed_id}" : "/"
+      json = MultiJson.load(response.body)
+      session['response_json'] = json
+      feed_id = json['feed_id']
+      api_key = json['apikey']
+      redirect feed_id ? "/egg/#{feed_id}/edit" : "/"
     rescue
       redirect '/'
     end
--- spec/airqualityegg_spec.rb
+++ spec/airqualityegg_spec.rb
@@ -11,25 +11,14 @@ describe AirQualityEgg do
   describe 'registering' do
 
     it 'should render the dashboard if found' do
-      request_stub = stub_request(:get, "http://api.cosm.com/v2/device_models/airqualityegg/devices/123").
+      request_stub = stub_request(:get, "http://api.cosm.com/v2/products/airqualityegg/devices/123/activate").
         with(:headers => { 'X-ApiKey' => 'apikey'}).
-        to_return(:status => 200, :body => '{"device":{"serial":"abcd","activated_at":null,"created_at":"2012-09-10T14:48:12Z","activation_code":"494ea17400d535a6824a8124d07cc3ba90a43556","feed_id":"45783"}}')
+        to_return(:status => 200, :body => MultiJson.dump({"datastreams"=>[], "feed_id"=>101, "apikey"=>"HSA8lzxDe-uOigbz8Ic_syfuGsaSAKxjcUZMS3NTbXJhWT0g"}))
       post '/register', :serial => '123'
       request_stub.should have_been_made
       last_response.should be_redirect
       follow_redirect!
-      last_request.url.should == "http://example.org/egg/45783"
-    end
-
-    it 'should handle unactivated eggs' do
-      request_stub = stub_request(:get, "http://api.cosm.com/v2/device_models/airqualityegg/devices/123").
-        with(:headers => { 'X-ApiKey' => 'apikey'}).
-        to_return(:status => 200, :body => '{"device":{"serial":"abcd","activated_at":null,"created_at":"2012-09-10T14:48:12Z","activation_code":"494ea17400d535a6824a8124d07cc3ba90a43556","feed_id":null}}')
-      post '/register', :serial => '123'
-      request_stub.should have_been_made
-      last_response.should be_redirect
-      follow_redirect!
-      last_request.url.should == "http://example.org/"
+      last_request.url.should == "http://example.org/egg/101/edit"
     end
 
     it 'should handle no serial' do
@@ -40,9 +29,9 @@ describe AirQualityEgg do
     end
 
     it 'should handle missing eggs' do
-      request_stub = stub_request(:get, "http://api.cosm.com/v2/device_models/airqualityegg/devices/123").
+      request_stub = stub_request(:get, "http://api.cosm.com/v2/products/airqualityegg/devices/123/activate").
         with(:headers => { 'X-ApiKey' => 'apikey'}).
-        to_return(:status => 404, :body => "{\"errors\":\"I'm sorry we are unable to find the device you are looking for.\",\"title\":\"Not found\"}")
+        to_return(:status => 404, :body => MultiJson.dump({"title"=>"Not found", "errors"=>"I'm sorry we are unable to find the device you are looking for."}))
       post '/register', :serial => '123'
       request_stub.should have_been_made
       last_response.should be_redirect
