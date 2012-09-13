93cc9be2216d5289fc7a2a77eed71a827f69cace
Pete Correia <pete.correia@logmein.com>
2012-09-13 16:23:07 +0000

    Fixed map markers

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -28,7 +28,7 @@ var AQE = (function ( $ ) {
 
     if ( mapmarkers && mapmarkers.length ) {
       for ( var x = 0, len = mapmarkers.length; x < len; x++ ) {
-        addMapMarker( mapmarkers[x].lat, mapmarkers[x].lat );
+        addMapMarker( mapmarkers[x].lat, mapmarkers[x].lng );
       }
     }
   }
@@ -42,33 +42,12 @@ var AQE = (function ( $ ) {
     var myLatlng = new google.maps.LatLng(lat, lng);
     var marker = new google.maps.Marker({
       position: myLatlng,
-      map: map,
-      title: 'Hello World!'
+      map: map
     });
   }
 
   if ( $(".home-map").length ) {
     google.maps.event.addDomListener(window, 'load', initialize);
-  }
-
-  //
-  // LOCATION PICKER
-  //
-
-  var locpicker = $(".gllpLatlonPicker").first();
-
-  if ( locpicker.length ) {
-    
-    (new GMapsLatLonPicker()).init( locpicker );
-
-    // HTML5 geolocation
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        $(".gllpLatitude").val(position.coords.latitude);
-        $(".gllpLongitude").val(position.coords.longitude);
-      });
-    }
-
   }
 
 })( jQuery );
